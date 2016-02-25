a221ddb69f26e62e872bdf843bb4187bb710cbaf
az0977776 <az0977776@yahoo.com>
2016-02-25 00:08:53 -0500

    i forgot what i changed

--- hw03/animate.js
+++ hw03/animate.js
@@ -1,25 +1,26 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 var mbutton = document.getElementById("move");
-var image = document.getElementById("image");
+var image = new Image();
+image.src = "dvd.png";
 
 var buttonState = false;
 var xchange = 1;
 var ychange = 1;
-var xpos = 250;
-var ypos = 250;
+var xpos = 100;
+var ypos = 200;
 
 var move = function() {
     ctx.clearRect(0,0,c.width,c.height);
-    if (xpos == 500 || xpos == 0) {
+    if (xpos == 500-image.width/20 || xpos == 0) {
 	xchange *=-1;
     }
-    if (ypos == 500 || ypos == 0) {
+    if (ypos == 500-image.height/20 || ypos == 0) {
 	ychange *=-1;
     }
     xpos+=xchange;
     ypos+=ychange;
-    ctx.drawImage(image,xpos,ypos);
+    ctx.drawImage(image,xpos,ypos,image.width/20,image.height/20);
     if (buttonState == true) {
 	window.requestAnimationFrame(move);
     }
@@ -28,12 +29,12 @@ var move = function() {
 var buttonclick = function() {
     if (buttonState == false) {
 	buttonState = true;
-	cbutton.innerHTML = "Stop";
+	mbutton.innerHTML = "Stop";
 	cid = window.requestAnimationFrame(move);
     }
     else if (buttonState == true) {
 	buttonState = false;
-	cbutton.innerHTML = "Start";
+	mbutton.innerHTML = "Start";
 	window.cancelAnimationFrame(cid);
     }
 };
