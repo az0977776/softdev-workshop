aa9cc44583172cc571017eb3239dc3ed72e89fc2
az0977776 <az0977776@yahoo.com>
2016-03-03 00:01:16 -0500

    xD

--- hw04/script.js
+++ hw04/script.js
@@ -8,10 +8,19 @@ var bRect = pic.getBoundingClientRect();
 
 var interval;
 
+var CActive = false;
+var DActive = false;
+
 var clear = function() {
     stop();
     radius = 0;
     change = 0;
+    xchange = 1;
+    ychange = 1;
+    xpos = 100;
+    ypos = 150;
+    CActive = false;
+    DActive = false;
     while (pic.childElementCount != 0) {
 	pic.removeChild(pic.children[0]);
     }
@@ -31,7 +40,6 @@ var drawC = function() {
     while(pic.childElementCount != 0) {
 	pic.removeChild(pic.children[0]);
     }
-    
     if (radius <= 0) {
 	change = 1;
     }
@@ -45,10 +53,11 @@ var drawC = function() {
     c.setAttribute("r", radius);
     c.setAttribute("fill", "green");
     c.setAttribute("stroke", "black");
-
+	
     pic.appendChild(c);
-    
+	
     radius += change;
+    
 };
 
 var drawDVD = function() {
@@ -57,31 +66,49 @@ var drawDVD = function() {
 	pic.removeChild(pic.children[0]);
     }
     
-    if (xpos == bRect.width - || xpos == 0) {
+    if (xpos == bRect.width - 200 || xpos == 0) {
 	xchange *= -1;
     }
-    if (ypos == bRect.width - || ypos == 0) {
+    if (ypos == bRect.width - 100 || ypos == 0) {
 	ychange *= -1;
     }
     xpos += xchange;
     ypos += ychange;
-
     
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "image");
+    c.setAttributeNS("http://www.w3.org/1999/xlink", "href", "dvd.png");
+    c.setAttributeNS(null, "x", xpos);
+    c.setAttributeNS(null, "y", ypos);
+    c.setAttributeNS(null, "height", "100");
+    c.setAttributeNS(null, "width", "200");
+    c.setAttributeNS(null, "visibility", "visible");
+
+    pic.appendChild(c);    
+
 };
 
 var stop = function() {
     clearInterval(interval);
+    DActive = false;
+    CActive = false;
 };
 
 var circleClick = function() {
-    interval = setInterval(drawC, 16);
+    if (CActive == false && DActive == false) {
+	CActive = true;
+	interval = setInterval(drawC, 16);
+    }
 };
 
 var DVDClick = function() {
-    interval = setInterval(drawDVD, 16);
+    if (DActive == false && CActive == false) {
+	DActive = true;
+	interval = setInterval(drawDVD, 16);
+    }
 };
 
 
+dbutton.addEventListener("click", DVDClick);
 sbutton.addEventListener("click", stop);
 cbutton.addEventListener("click", circleClick);
 clearbutton.addEventListener("click", clear);
